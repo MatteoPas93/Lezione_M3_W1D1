821fb1c1ca31dc5da3c5158f2cc1807432044611
MatteoPas93 <pascucci-matteooutlook.it>
2023-11-15 21:11:48 +0100

    Fine Compito

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 // Esercizio n°1
 
-let twitter = document.querySelector(".col-md-4.blog-sidebar .p-4:last-of-type .list-unstyled:first-of-type li:nth-of-type(2) ")
-console.log(twitter)
+let twitter = document.querySelector(
+  ".col-md-4.blog-sidebar .p-4:last-of-type .list-unstyled:first-of-type li:nth-of-type(2) "
+);
+console.log(twitter);
 const removeTwitter = function () {
-    twitter.remove()
-}
-removeTwitter()
+  twitter.remove();
+};
+removeTwitter();
 
 // Esercizio n°2
 
-const linkForElimination = document.querySelector(".position-static:first-of-type")
-const link = document.querySelector(".position-static:first-of-type a:first-of-type")
+const linkForElimination = document.querySelector(
+  ".position-static:first-of-type"
+);
+const link = document.querySelector(
+  ".position-static:first-of-type a:first-of-type"
+);
 
-link.addEventListener('click', function() {
-    linkForElimination.parentNode.removeChild(linkForElimination)
-})
-
+link.addEventListener("click", function () {
+  linkForElimination.parentNode.removeChild(linkForElimination);
+});
 
 //    Esercizio n°3
 
 function alertName(ev) {
-    let author = ev.target.textContent
-    alert (`Author: ${author}`)
+  let author = ev.target.innerText;
+  alert(`Author: ${author}`);
 }
 
-    let authorName = document.querySelectorAll(".blog-post-meta a")
-    authorName.forEach(function (name) {
-        name.addEventListener("mouseover", alertName)
-    })
-
-
-
-
+let authorName = document.querySelectorAll(".blog-post-meta a");
+authorName.forEach(function (name) {
+  name.addEventListener("mouseover", alertName);
+});
